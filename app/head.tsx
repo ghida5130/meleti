a655e1656d634350de68be7ffabf06f97c7d4d48
unknown <ghida5130@gmail.com>
2024-12-19 18:35:38 +0900

    fix manifest

--- app/head.tsx
+++ app/head.tsx
@@ -4,8 +4,10 @@ export default function Head() {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link rel="manifest" href="/manifest.json" />
             <meta name="theme-color" content="#000000" />
-            <link rel="icon" href="/icons/web-app-manifest-192x192.png" />
-            <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" href="/public/icons/favicon-96x96.png" sizes="96x96" />
+            <link rel="icon" type="image/svg+xml" href="/public/icons/favicon.svg" />
+            <link rel="shortcut icon" href="/public/icons/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/public/icons/apple-touch-icon.png" />
         </>
     );
 }
