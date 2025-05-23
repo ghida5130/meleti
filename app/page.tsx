f4cd6ad707c140533bda95d643941d61998cdfec
ghida5130 <ghida5130@gmail.com>
2025-05-23 23:41:47 +0900

    vercel toolbar 제거

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,6 @@ export default async function Home() {
             <div className={styles.bannerArea}>
                 <Image src={testImage} alt="" fill style={{ objectFit: "cover" }} />
             </div>
-            {/* <div className={styles.mainPageSearchArea}>
-                <Link href="/search" className={styles.mainPageSearchBtn}>
-                    <Image src={searchBtn} alt="searchButton" width={20} style={{ marginRight: "10px" }} />
-                    검색
-                </Link>
-            </div> */}
             <Content />
         </div>
     );
