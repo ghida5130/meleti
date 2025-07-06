390297064b0c26ef4b84feac6a9a9c3a70ad2233
ghida5130 <ghida5130@gmail.com>
2025-07-06 18:08:46 +0900

    fix:lhci 재테스트

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
     ci: {
         collect: {
             url: ["http://localhost:3000/book/9788936434595"], // 한강 - 채식주의자 페이지의 lighthouse ci
-            startServerCommand: "npm run build && npm start && node warmup.mjs",
+            startServerCommand: "npm start",
             waitForReady: true,
             readyPattern: "Ready",
             numberOfRuns: 2,
