9dbc214de61847b05f4b5bd1f5ad523f49aa0140
ghida5130 <ghida5130@gmail.com>
2025-07-06 19:55:36 +0900

    fix: lighthouseci 설정 마무리

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,7 +5,24 @@ module.exports = {
             startServerCommand: "npm start",
             waitForReady: true,
             readyPattern: "Ready",
-            numberOfRuns: 2,
+            numberOfRuns: 1,
+            settings: {
+                formFactor: "mobile",
+                // 모바일 환경 기준 측정
+                screenEmulation: {
+                    mobile: true,
+                    width: 393,
+                    height: 852,
+                    deviceScaleFactor: 3,
+                    disabled: false,
+                },
+                throttlingMethod: "devtools",
+                throttling: {
+                    rttMs: 75, // LTE 기준 지연시간
+                    throughputKbps: 10000, // 약 1MB/s
+                    cpuSlowdownMultiplier: 2,
+                },
+            },
         },
         upload: {
             target: "temporary-public-storage",
